5c00f797bcee884d0e3c92a5c710a75f8a58daa2
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-11 13:47:24 -0800

    GET /api/posts/:id working

--- README.md
+++ README.md
@@ -135,7 +135,7 @@ When the client makes a `POST` request to `/api/posts/:id/comments`:
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The posts information could not be retrieved." }`.
 
-When the client makes a `GET` request to `/api/posts/:id`:
+✅When the client makes a `GET` request to `/api/posts/:id`:
 
 - If the _post_ with the specified `id` is not found:
 
--- blogRouter.js
+++ blogRouter.js
@@ -3,7 +3,7 @@ const database = require("./data/db");
 
 const router = express.Router();
 
-// GET all posts in database
+// GET all posts in database: /api/posts
 router.get("/", (req, res) => {
     database.find()
         .then(data => {
@@ -15,6 +15,31 @@ router.get("/", (req, res) => {
             res.status(500).json({ error: "The posts information could not be retrieved." });
         })
 })
+
+// GET post by id: /api/posts/:id
+router.get("/:id", (req, res) => {
+
+    const {id} = req.params;
+
+    database.findById(id)
+        .then(data => {
+
+            console.log("GET post with id", id, ":", data);
+
+            if (!data)
+                { res.status(404).json({ message: "The post with the specified ID does not exist." }) }
+            else
+                {
+                    console.log("Post with id", id, " found:", data);
+                    res.status(200).json(data);
+                }
+        })
+        .catch(error => {
+            console.log("Error (GET post with id", id, "):", error);
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
+})
+
 
 
 module.exports = router;
